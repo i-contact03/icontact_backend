005b58e0372875cf1bcb719265f0dfc8b9a6b520
Ahnhyungi <best21hg@naver.com>
2023-07-14 20:33:33 +0900

    07-14 안현기 커밋 로그인기능

--- src/com/icontact/user/controller/CheckEmailOkController.java
+++ src/com/icontact/user/controller/CheckEmailOkController.java
@@ -1,19 +1,39 @@
 package com.icontact.user.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.icontact.Action;
 import com.icontact.Result;
+import com.icontact.user.dao.UserDAO;
 
 public class CheckEmailOkController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=utf-8");
+		String memberEmail = req.getParameter("memberEmail");
+		UserDAO memberDAO = new UserDAO();
+		boolean check = memberDAO.selectEmail(memberEmail) == null;
+		JSONObject result = new JSONObject();
+		
+		try {
+			result.put("check", check);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		PrintWriter out = resp.getWriter();
+		out.print(result.toString());
+		out.close();
+		
 		return null;
 	}
 
--- src/com/icontact/user/controller/CheckIdOkController.java
+++ src/com/icontact/user/controller/CheckIdOkController.java
@@ -1,19 +1,39 @@
 package com.icontact.user.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.icontact.Action;
 import com.icontact.Result;
+import com.icontact.user.dao.UserDAO;
 
 public class CheckIdOkController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		UserDAO memberDAO = new UserDAO();
+		String memberIdentification = memberDAO.selectIdentification(req.getParameter("memberIdentification"));
+		boolean check = memberIdentification == null;
+		
+		JSONObject result = new JSONObject();
+		try {
+			result.put("check", check);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		resp.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.print(result.toString());
+		out.close();
 		return null;
 	}
 
