31cd5309970ea1a2aad28484034e9f257c5b1d26
dev-johnY <dev.johnyoon@gamil.com>
2023-07-15 05:58:45 +0900

    0715 윤재훈 프로필메인 일부분 완성

---
+++ src/com/icontact/interest/domain/InterestVO.java
@@ -0,0 +1,67 @@
+package com.icontact.interest.domain;
+
+public class InterestVO {
+
+	private Long interestId;
+	private Long userId;
+	private Long scId;
+
+	public InterestVO() {;}
+
+	public Long getInterestId() {
+		return interestId;
+	}
+
+	public void setInterestId(Long interestId) {
+		this.interestId = interestId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getScId() {
+		return scId;
+	}
+
+	public void setScId(Long scId) {
+		this.scId = scId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((interestId == null) ? 0 : interestId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InterestVO other = (InterestVO) obj;
+		if (interestId == null) {
+			if (other.interestId != null)
+				return false;
+		} else if (!interestId.equals(other.interestId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "InterestVO [interestId=" + interestId + ", userId=" + userId + ", scId=" + scId + "]";
+	}
+	
+	
+	
+}
