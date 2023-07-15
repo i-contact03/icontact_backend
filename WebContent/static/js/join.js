a850f9dff5f07f945d367ba6995b44d164708f6d
Ahnhyungi <best21hg@naver.com>
2023-07-15 16:47:20 +0900

    커밋

--- WebContent/static/js/join.js
+++ WebContent/static/js/join.js
@@ -640,7 +640,7 @@ let modalMessage = `
 								<div class="login-main-login-below-wrapper">	<!-- css-70qvj9 equrxy92 -->
 									<label color="#ffd400" class="login-main-login-below-check-for-keeplogin-wrapper">	<!-- css-18pqe3v elgfge0 -->
 										<span class="login-main-login-below-check-for-keeplogin-span">	<!-- css-9ffs86 elgfge3 -->
-											<input type="checkbox" name="remember" class="login-main-login-below-check-for-keeplogin-checkbox">	<!-- css-l775cp elgfge2 -->
+											<input type="checkbox" name="reuser" class="login-main-login-below-check-for-keeplogin-checkbox">	<!-- css-l775cp elgfge2 -->
 											<span color="#ffd400" class="login-main-login-below-check-for-keeplogin-checkbox-inner"></span>	<!-- checkbox__inner css-1t5nui elgfge4 -->
 											<span class="login-main-login-below-check-for-keeplogin-checkbox-textbox">	<!-- css-2k2mpm elgfge1 -->
 												<p variant="body2" class="login-main-login-below-p-tag" color="gray600">로그인 상태 유지</p>	<!-- css-15suf60 e870mj50 -->
