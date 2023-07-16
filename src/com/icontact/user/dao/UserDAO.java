2559fbebc3184eb9db14389fb228f500e03c6470
dev-johnY <dev.johnyoon@gamil.com>
2023-07-16 15:34:13 +0900

    0716 userMapper, userDAO 수정  윤재훈

--- src/com/icontact/user/dao/UserDAO.java
+++ src/com/icontact/user/dao/UserDAO.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.app.reply.domain.ReplyVO;
 import com.icontact.mybatis.config.MyBatisConfig;
+import com.icontact.user.domain.UserDTO;
 import com.icontact.user.domain.UserVO;
 
 
@@ -47,6 +49,13 @@ public class UserDAO {
 		
 		return sqlSession.selectOne("user.login", loginMap);
 	}
+	
+	
+//	프로필 수정
+	public void update(UserDTO userDTO) {
+		sqlSession.update("user.update", userDTO);
+	}
+	
 }
 
 
