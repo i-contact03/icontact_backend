00072173f06f02ff1874e322c3a515141e3d54c2
Ahnhyungi <best21hg@naver.com>
2023-07-22 20:08:38 +0900

    07-22 메인페이지 안현기커밋

--- src/com/icontact/user/controller/LoginController.java
+++ src/com/icontact/user/controller/LoginController.java
@@ -18,6 +18,7 @@ public class LoginController implements Action {
 		Result result = new Result();
 		boolean autoLogin = false;
 		
+		
 //		캐시: 내 PC의 메모리
 //		쿠키: 로컬에 저장
 //		세션: 서버에 저장
@@ -44,7 +45,7 @@ public class LoginController implements Action {
 			}
 		}
 		
-		System.out.println(autoLogin);
+		
 //		쿠키에 사용자 아이디가 있다면
 		if(userIdentification != null) {
 //			로그인으로 쿠키에 있던 아이디와 비밀번호 전송
@@ -52,12 +53,13 @@ public class LoginController implements Action {
 			req.setAttribute("userPassword", userPassword);
 			result.setPath("loginOk.user");
 		}else {
+			
 //			로그아웃 후 로그인 페이지로 이동한다면,
 //			자동 로그인 체크 여부를 판단하여 그대로 유지한다.
 			if(autoLogin) {
 				req.setAttribute("autoLogin", autoLogin);
 			}
-			result.setPath("templates/main/login_real.jsp");
+			result.setPath("mainAllList.main");
 		}
 		
 		return result;
